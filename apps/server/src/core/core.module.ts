ef216c71323a4286611bfadc47a27019f0cf9cb4
Victor Nguyen <ngmthaq.20@gmail.com>
2025-12-14 09:21:29 +0700

    feat: init event

--- apps/server/src/core/core.module.ts
+++ apps/server/src/core/core.module.ts
@@ -4,12 +4,14 @@ import { CoreConfigModule } from './config/config.module';
 import { CoreCacheModule } from './cache/cache.module';
 import { CoreScheduleModule } from './schedule/schedule.module';
 import { CoreQueueModule } from './queue/queue.module';
+import { CoreEventModule } from './event/event.module';
 
 const coreModules = [
   CoreConfigModule,
   CoreCacheModule,
   CoreScheduleModule,
   CoreQueueModule,
+  CoreEventModule,
 ];
 
 @Module({
---
+++ apps/server/src/core/event/event.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+
+/**
+ * Core event module that sets up event handling for the application.
+ * @see: https://docs.nestjs.com/techniques/events
+ */
+@Module({
+  imports: [
+    EventEmitterModule.forRoot({
+      // set this to `true` to use wildcards
+      wildcard: false,
+
+      // the delimiter used to segment namespaces
+      delimiter: '.',
+
+      // set this to `true` if you want to emit the newListener event
+      newListener: false,
+
+      // set this to `true` if you want to emit the removeListener event
+      removeListener: false,
+
+      // the maximum amount of listeners that can be assigned to an event
+      maxListeners: 10,
+
+      // show event name in memory leak message when more than maximum amount of listeners is assigned
+      verboseMemoryLeak: false,
+
+      // disable throwing uncaughtException if an error event is emitted and it has no listeners
+      ignoreErrors: false,
+    }),
+  ],
+  exports: [EventEmitterModule],
+})
+export class CoreEventModule {}
