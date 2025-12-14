c9bec9c75b9c9553bba2da44b62d16e78e5cab9e
Victor Nguyen <ngmthaq.20@gmail.com>
2025-12-14 09:10:10 +0700

    feat: prepare for queue setup

--- apps/server/src/core/core.module.ts
+++ apps/server/src/core/core.module.ts
@@ -3,8 +3,14 @@ import { Module } from '@nestjs/common';
 import { CoreConfigModule } from './config/config.module';
 import { CoreCacheModule } from './cache/cache.module';
 import { CoreScheduleModule } from './schedule/schedule.module';
+import { CoreQueueModule } from './queue/queue.module';
 
-const coreModules = [CoreConfigModule, CoreCacheModule, CoreScheduleModule];
+const coreModules = [
+  CoreConfigModule,
+  CoreCacheModule,
+  CoreScheduleModule,
+  CoreQueueModule,
+];
 
 @Module({
   imports: coreModules,
---
+++ apps/server/src/core/queue/queue.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * Core queue module that sets up queuing for the application.
+ * TODO: bullmq already installed, implement queue setup if needed.
+ * @see: https://docs.nestjs.com/techniques/queues
+ */
+@Module({
+  imports: [],
+  exports: [],
+})
+export class CoreQueueModule {}
